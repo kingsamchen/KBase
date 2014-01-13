aea1b6c32f30294a95605bb2ea197d27041f2f00
Kingsley Chen <kingsamchen@gmail.com>
2014-01-13 18:20:42 +0800

    string_piece: add files

---
+++ strings/string_piece.cpp
@@ -0,0 +1,11 @@
+#include "string_piece.h"
+
+
+BasicStringPiece::BasicStringPiece(void)
+{
+}
+
+
+BasicStringPiece::~BasicStringPiece(void)
+{
+}
---
+++ strings/string_piece.h
@@ -0,0 +1,8 @@
+#pragma once
+class BasicStringPiece
+{
+public:
+    BasicStringPiece(void);
+    ~BasicStringPiece(void);
+};
+
