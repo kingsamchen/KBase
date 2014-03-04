477bb47dca2655d47e7796a571127e1cb25ec853
kingsamchen <kingsamchen@gmail.com>
2014-03-05 00:17:25 +0800

    set up sys_string_encoding_conversions

---
+++ strings/sys_string_encoding_conversions.cpp
@@ -0,0 +1,9 @@
+/*
+ @ Kingsley Chen
+*/
+
+#include "sys_string_encoding_conversions.h"
+
+namespace KBase {
+
+}   // namespace KBase
---
+++ strings/sys_string_encoding_conversions.h
@@ -0,0 +1,16 @@
+/*
+ @ Kingsley Chen
+*/
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#ifndef KBASE_STRINGS_SYS_STRING_ENCODING_CONVERSIONS_H_
+#define KBASE_STRINGS_SYS_STRING_ENCODING_CONVERSIONS_H_
+
+namespace KBase {
+
+}   // namespace KBase
+
+#endif  // KBASE_STRINGS_SYS_STRING_ENCODING_CONVERSIONS_H_
