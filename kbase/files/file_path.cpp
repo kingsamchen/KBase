d2205a85b7a01781c807681013fd65d472006cb4
Kingsley Chen <kingsamchen@gmail.com>
2014-05-13 20:41:28 +0800

    kbase::file_path initial

---
+++ kbase/files/file_path.cpp
@@ -0,0 +1,132 @@
+/*
+ @ Kingsley Chen
+*/
+
+#include "kbase/files/file_path.h"
+
+#include <cassert>
+#include <cstdlib>
+
+#include <algorithm>
+#include <functional>
+#include <stdexcept>
+
+namespace kbase {
+
+typedef FilePath::PathChar PathChar;
+typedef FilePath::PathString PathString;
+
+const FilePath::PathChar FilePath::kSeparators[] = L"\\/";
+const size_t FilePath::kSeparatorsLength = _countof(FilePath::kSeparators);
+const FilePath::PathChar FilePath::kCurrentDir[] = L".";
+const FilePath::PathChar FilePath::kParentDir[] = L"..";
+const FilePath::PathChar FilePath::kExtensionSeparator = L'.';
+
+namespace {
+
+/*
+ @ If the |path| contains a drive letter specification, returns the position of the
+   last character of the specification; otherwise, return npos.
+*/
+PathString::size_type FindDriveLetter(const PathString& path)
+{
+    if (path.length() >= 2 && path[1] == L':' &&
+        ((path[0] >= L'A' && path[0] <= L'Z') ||
+         (path[0] >= L'a' && path[0] <= L'z'))) {
+        return 1;
+    }
+
+    return PathString::npos;
+}
+
+}   // namespace
+
+FilePath::FilePath()
+{}
+
+FilePath::FilePath(const FilePath& other)
+ : path_(other.path_)
+{}
+
+FilePath::FilePath(const PathString& path)
+ : path_(path)
+{
+    //TODO: what if path has someting after \0 ?
+}
+
+FilePath& FilePath::operator=(const FilePath& other)
+{
+    path_ = other.path_;
+    return *this;
+}
+
+FilePath::~FilePath()
+{}
+
+// static
+bool FilePath::IsSeparator(PathChar ch)
+{
+    using std::placeholders::_1;
+    using std::equal_to;
+
+    return std::any_of(std::begin(kSeparators), std::end(kSeparators),
+                       std::bind(equal_to<PathChar>(), ch, _1));
+}
+
+void FilePath::StripTrailingSeparatorsInternal()
+{
+    // start always points to the position one-offset past the leading slash
+    PathString::size_type start = FindDriveLetter(path_) + 2;
+
+    PathString::size_type last_stripped = PathString::npos;
+    PathString::size_type pos = path_.length();
+    for (; pos > start && IsSeparator(path_[pos-1]); --pos) {
+        if (pos != start + 1 ||
+            !IsSeparator(path_[start-1]) || 
+            last_stripped != PathString::npos) {
+            last_stripped = pos;
+        } else {
+            break;
+        }
+    }
+
+    path_.resize(pos);
+}
+
+FilePath FilePath::StripTrailingSeparators() const
+{
+    FilePath new_path(path_);
+    new_path.StripTrailingSeparatorsInternal();
+
+    return new_path;
+}
+
+void FilePath::GetComponents(std::vector<PathString>* components) const
+{
+    assert(components != nullptr);
+    if (!components) {
+        throw std::invalid_argument("components cannot be NULL!");
+    }
+
+    components->clear();
+
+    if (path_.empty()) {
+        return;
+    }
+
+    //size_t slash_pos = path_.find_first_of(kSeparators);
+    //if (slash_pos == 0) {
+    //    components->emplace_back(1, path_[slash_pos]);
+    //} else if (path_[slash_pos-1] == L':') {
+    //    components->emplace_back(path_.substr(0, slash_pos));
+    //    components->emplace_back(1, path_[slash_pos]);
+    //}
+
+    //std::vector<std::wstring> parts;
+    //kbase::Tokenize(path_.substr(slash_pos + 1), kSeparators, &parts);
+
+    //std::remove_copy(parts.cbegin(), parts.cend(), std::back_inserter(*components),
+    //                 kCurrentDir);
+}
+
+}   // namespace kbase
---
+++ kbase/files/file_path.h
@@ -0,0 +1,80 @@
+/*
+ @ Kingsley Chen
+*/
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef KBASE_FILES_FILE_PATH_H_
+#define KBASE_FILES_FILE_PATH_H_
+
+#include <string>
+#include <vector>
+
+namespace kbase {
+
+class FilePath {
+public:
+    typedef std::wstring PathString;
+    typedef PathString::value_type PathChar;
+
+    // separators in path hierarchy. supports both '/' and '\'
+    static const PathChar kSeparators[];
+    static const size_t kSeparatorsLength;
+
+    static const PathChar kCurrentDir[];
+    static const PathChar kParentDir[];
+    static const PathChar kExtensionSeparator;
+
+    FilePath();
+    
+    FilePath(const FilePath& other);
+
+    explicit FilePath(const PathString& path);
+
+    FilePath& operator=(const FilePath& other);
+
+    ~FilePath();
+
+    //TODO: some comparison operators
+
+    const PathString& value() const
+    {
+        return path_;
+    }
+
+    bool empty() const
+    {
+        return path_.empty();
+    }
+
+    void clear()
+    {
+        path_.clear();
+    }
+
+    // Checks whether |ch| is in kSeparators.
+    static bool IsSeparator(PathChar ch);
+
+    // This function internally calls StripTrailingSeparatorInternal, which returns
+    // the path that has been stripped.
+    FilePath StripTrailingSeparators() const;
+
+    FilePath DirName() const;
+
+    //TODO: GetComponents
+    void GetComponents(std::vector<PathString>* components) const;
+    //TODO: IsParent
+    bool IsParent(const FilePath& child) const;
+
+private:
+    void StripTrailingSeparatorsInternal();
+
+private:
+    PathString path_;
+};
+
+}   // namespace kbase
+
+#endif  // KBASE_FILES_FILE_PATH_H_
