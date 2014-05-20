2b474d4cbb10a235b04ac4f794a9b27842dc9ce1
Kingsley Chen <kingsamchen@gmail.com>
2014-05-20 11:43:16 +0800

    file_path: done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+﻿
 #include <cassert>
 #include <cstdio>
 #include <conio.h>
@@ -23,52 +23,7 @@ void print_out(T beg, T end, const char* dem = " ")
 
 int main(int /*argc*/, char* /*argv[]*/)
 {
-    std::wstring data[] = {
-        L"",
-        L"aa",
-        L"/aa/bb",
-        L"/aa/bb/",
-        L"/aa/bb//",
-        L"/aa/bb/ccc",
-        L"/aa",
-        L"/",
-        L"//",
-        L"///",
-        L"aa/",
-        L"aa/bb",
-        L"aa/bb/",
-        L"aa/bb//",
-        L"aa//bb//",
-        L"aa//bb/",
-        L"aa//bb",
-        L"//aa/bb",
-        L"//aa/",
-        L"//aa",
-        L"0:",
-        L"@:",
-        L"[:",
-        L"`:",
-        L"{:"
-    };
-
-    for (const auto& s : data) {
-        kbase::FilePath path(s);
-        std::wcout << s << L"\t-->\t" << path.BaseName().value() << std::endl;
-    }
 
     _getch();
     return 0;
 }
-
-#ifdef _DEBUG
-namespace kbase {
-//void _pickle_dump(const Pickle& pk)
-//{
-//    cout << "capacity:" << pk.capacity_ << endl
-//         << "header:" << std::hex << pk.header_ << endl
-//         << "payload addr:" << reinterpret_cast<int>(pk.payload()) << endl << std::dec
-//         << "payload size:" << pk.header_->payload_size << endl
-//         << "buffer_offset:" << pk.buffer_offset_ << endl;
-//}
-}
-#endif
