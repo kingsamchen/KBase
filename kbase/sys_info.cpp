77f72f2742b0a3e2294388945685e7049cd556a6
Kingsley Chen <kingsamchen@gmail.com>
2014-07-29 00:24:54 +0800

    modified sys_info
    1.+ NumberOfProcessors()
    2.+ AmountOfTotalPhysicalMemory()
    3.+ AmountOfAvailablePhysicalMemory()
    4.+ AmountOfTotalVirtualMemory()
    5.+ AmountOfAvailableVirtualMemory()

--- kbase/sys_info.cpp
+++ kbase/sys_info.cpp
@@ -4,7 +4,54 @@
 
 #include "sys_info.h"
 
+#include <Windows.h>
+
+#include "kbase\error_exception_util.h"
+#include "kbase\version_util.h"
+
 namespace kbase {
 
+namespace {
+
+uint64_t AmountOfMemory(DWORDLONG MEMORYSTATUSEX::* memory_field)
+{
+    MEMORYSTATUSEX memory_status = { sizeof(memory_status) };
+    BOOL ret = GlobalMemoryStatusEx(&memory_status);
+    ThrowLastErrorIf(!ret, "Failed to get global memory status!");
+
+    return memory_status.*memory_field;
+}
+
+}   // namespace
+
+// static
+unsigned long SysInfo::NumberOfProcessors()
+{
+    return OSInfo::GetInstance()->processors();
+}
+
+// static
+uint64_t SysInfo::AmountOfTotalPhysicalMemory()
+{
+    return AmountOfMemory(&MEMORYSTATUSEX::ullTotalPhys);
+}
+
+// static
+uint64_t SysInfo::AmountOfAvailablePhysicalMemory()
+{
+    return AmountOfMemory(&MEMORYSTATUSEX::ullAvailPhys);
+}
+
+// static
+uint64_t SysInfo::AmountOfTotalVirtualMemory()
+{
+    return AmountOfMemory(&MEMORYSTATUSEX::ullTotalVirtual);
+}
+
+// static
+uint64_t SysInfo::AmountOfAvailableVirtualMemory()
+{
+    return AmountOfMemory(&MEMORYSTATUSEX::ullAvailVirtual);
+}
 
 }   // namespace kbase
--- kbase/sys_info.h
+++ kbase/sys_info.h
@@ -9,12 +9,32 @@
 #ifndef KBASE_SYS_INFO_H_
 #define KBASE_SYS_INFO_H_
 
+#include <cstdint>
+
 namespace kbase {
 
 class SysInfo {
 public:
     SysInfo() = delete;
+    ~SysInfo() = delete;
 
+    // Returns the number of logical cores on the machine.
+    static unsigned long NumberOfProcessors();
+
+    // Returns the amount of actual physical memory, in bytes.
+    static uint64_t AmountOfTotalPhysicalMemory();
+
+    // Returns the amount of physical memory currently available, in bytes.
+    static uint64_t AmountOfAvailablePhysicalMemory();
+
+    // Returns the size of the user mode portion of the virtual address space of the 
+    // calling process, in bytes. This value may vary in different type of process,
+    // processors, or configurations of the system.
+    static uint64_t AmountOfTotalVirtualMemory();
+
+    // Returns the amount of unreserved and uncommitted memory currently in user mode
+    // portion of the virtual address space of the calling process, in bytes.
+    static uint64_t AmountOfAvailableVirtualMemory();
 };
 
 }   // namespace kbase
