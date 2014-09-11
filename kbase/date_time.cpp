d7e2b8152d8772b4c42cc10f16ad06c9630e32e0
Kingsley Chen <kingsamchen@gmail.com>
2014-09-11 09:07:59 +0800

    add date_time
    DateTime ctor(not finished)

--- kbase/date_time.cpp
+++ kbase/date_time.cpp
@@ -4,6 +4,50 @@
 
 #include "kbase\date_time.h"
 
+#include <cassert>
+#include <stdexcept>
+
 namespace kbase {
+
+DateTime::DateTime(time_t time)
+    : time_(time)
+{}
+
+DateTime::DateTime(int year, int month, int day)
+    : DateTime(year, month, day, 0, 0, 0)
+{}
+
+DateTime::DateTime(int year, int month, int day, int hour, int min, int sec)
+{
+    assert(year >= 1900);
+    assert(month >= 1 && month <= 12);
+    assert(day >= 1 && day <= 31);
+    assert(hour >= 0 && hour <= 23);
+    assert(min >= 0 && min <= 59);
+    assert(sec >= 0 && sec <= 59);
+
+    struct tm std_tm;
+    std_tm.tm_year = year - 1900;
+    std_tm.tm_mon = month - 1;
+    std_tm.tm_mday = day;
+    std_tm.tm_hour = hour;
+    std_tm.tm_min = min;
+    std_tm.tm_sec = sec;
+    std_tm.tm_isdst = -1;
+
+    time_ = _mktime64(&std_tm);
+    if (time_ == -1) {
+        throw std::invalid_argument("failed to convert to DateTime");
+    }
+}
+
+DateTime::DateTime(const SYSTEMTIME& systime)
+{
+    if (systime.wYear < 1900) {
+        throw std::invalid_argument("failed to convert to DateTime");
+    }
+
+    // TODO: make DateTime from systime
+}
 
 }   // namespace kbase
--- kbase/date_time.h
+++ kbase/date_time.h
@@ -9,12 +9,34 @@
 #ifndef KBASE_DATE_TIME_H_
 #define KBASE_DATE_TIME_H_
     
-namespace kbase {
+#include <WinBase.h>
 
+#include <ctime>
+
+namespace kbase {
+namespace internal {
+
+typedef __time64_t time_type;
+
+}   // namespace internal
+
+// This class is intended to be designed as of value type.
 class DateTime {
 public:
-    DateTime();
-    ~DateTime();
+    explicit DateTime(time_t time);
+
+    DateTime(int year, int month, int day);
+
+    DateTime(int year, int month, int day, int hour, int min, int sec);
+
+    explicit DateTime(const SYSTEMTIME& systime);
+
+    explicit DateTime(const FILETIME& filetime);
+
+    static DateTime Now();
+
+private:
+    internal::time_type time_;
 };
 
 }   // namespace kbase
