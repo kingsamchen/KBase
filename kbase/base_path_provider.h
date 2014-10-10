105fba76b00cf5f80b907f3029b83bf08c902d97
Kingsley Chen <kingsamchen@gmail.com>
2014-10-11 00:59:56 +0800

    add fundamental structures and data

---
+++ kbase/base_path_provider.h
@@ -0,0 +1,21 @@
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef KBASE_BASE_PATH_PROVIDER_H_
+#define KBASE_BASE_PATH_PROVIDER_H_
+
+#include "kbase\path_service.h"
+
+namespace kbase {
+
+enum : PathService::PathKey {
+    BASE_PATH_START = 0,
+    // TODO: add more keys
+    BASE_PATH_END
+};
+
+}   // namespace kbase
+
+#endif // KBASE_BASE_PATH_PROVIDER_H_
--- kbase/path_service.cpp
+++ kbase/path_service.cpp
@@ -1,21 +1,58 @@
 
-#include "kbase/path_service.h"
+#include "kbase\path_service.h"
 
+#include <forward_list>
+#include <mutex>
 #include <unordered_map>
 
-namespace {
+#include "kbase\base_path_provider.h"
 
 using kbase::FilePath;
 typedef kbase::PathService::PathKey PathKey;
+
+namespace kbase {
+
+FilePath BasePathProvider(PathKey);
+
+}   // namespace kbase
+
+namespace {
+
+typedef kbase::PathService::ProviderFunc ProviderFunc;
 typedef std::unordered_map<PathKey, FilePath> PathMap;
 
+// Both |start| and |end| are used to prevent path keys claimed by different provides
+// being overlapped.
 struct PathProvider {
-
+    ProviderFunc fn;
+    PathKey start;
+    PathKey end;
 };
+
+// We keep provides in a linked list.
+typedef std::forward_list<PathProvider> ProviderChain;
 
 struct PathData {
+    ProviderChain providers;
+    std::mutex lock;
+    PathMap cached_path_table;
+    bool cache_disabled;
 
+    PathData()
+        : providers {{ PathProvider { kbase::BasePathProvider,
+                                      kbase::BASE_PATH_START,
+                                      kbase::BASE_PATH_END }}},
+          cache_disabled(false)
+    {}
 };
+
+// TODO: replace with LazyInstance object
+PathData g_path_data;
+
+PathData& path_data()
+{
+    return g_path_data;
+}
 
 }   // namespace
 
