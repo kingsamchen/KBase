4198133e38de28fbdea4f2360f8be47c1925fa22
Kingsley Chen <kingsamchen@gmail.com>
2014-11-05 00:16:07 +0800

    done with ScopedHandle.

---
+++ KBase_Test/samples/scoped_handle_unittest.cpp
@@ -0,0 +1,15 @@
+
+#include "stdafx.h"
+
+#include "gtest\gtest.h"
+#include "kbase\memory\scoped_handle.h"
+
+TEST(ScopedHandleTest, TestMain)
+{
+    
+}
+
+TEST(ScopedHandleTest, Validness)
+{
+ 
+}
--- kbase/memory/scoped_handle.h
+++ kbase/memory/scoped_handle.h
@@ -1,3 +1,6 @@
+/*
+ @ Kingsley Chen
+*/
 
 #if defined(_MSC_VER)
 #pragma once
@@ -57,29 +60,6 @@ struct HandleTraits<FILE*> {
     }
 };
 
-/*
-    - Dev Thoughts -
-
-    ScopedHandle() -> auto-nullize the handle member
-    explicit ScopedHandle(Handle) -> construct with a given handle value.
-    ScopedHandle(Handle&&) -> move-ctor. transfer ownership.
-    ~ScopedHandle() -> release handle if necessary.
-    ScopedHandle& operator=(nullptr_t) -> explicit release.
-    ScopedHandle& operator=(Handle&&) -> move-assign. transfer ownership.
-
-    Release() -> explicit release.
-    Reset() -> replace the handle.
-    swap() -> swap handle.
-    Close()
-
-    Get()
-    operator bool
-    operator Handle
-
-    -- non-member --
-    std::hash<ScopedHandle>
-    std::swap
-*/
 template<typename HandleType, typename TraitsType = HandleTraits<HandleType>>
 class ScopedHandle {
 public:
@@ -114,10 +94,21 @@ public:
         return *this;
     }
 
+    // A convenient way for destroying the object.
     ScopedHandle& operator=(nullptr_t)
     {
         Close();
         return *this;
+    }
+
+    explicit operator bool() const
+    {
+        return Traits::IsValid(handle_);
+    }
+
+    operator Handle() const
+    {
+        reutrn handle_;
     }
 
     void Close()
@@ -128,14 +119,47 @@ public:
         }
     }
 
+    // Releases the ownership of the managed handle.
+    // Returns managed handle or NullHandle if there was no managed object.
+    Handle Release()
+    {
+        Handle self = handle_;
+        handle_ = Traits::NullHandle();
+
+        return self;
+    }
+
     Handle Get() const
     {
         return handle_;
     }
 
+    // Replaces the managed handle.
+    void Reset(Handle new_handle)
+    {
+        Close();
+        handle_ = new_handle;
+    }
+
+    void swap(ScopedHandle& other)
+    {
+        using std::swap;
+        swap(handle_, other.handle_);
+    }
+
 private:
     Handle handle_;
 };
+
+// Specialized version for std::swap.
+template<typename HandleType, typename TraitsType = HandleTraits<HandleType>>
+void swap(ScopedHandle<HandleType>& lhs, ScopedHandle<HandleType>& rhs)
+{
+    lhs.swap(rhs);
+}
+
+typedef ScopedHandle<HANDLE> ScopedSysHandle;
+typedef ScopedHandle<FILE*> ScopedStdioHandle;
 
 }   // namespace kbase
 
