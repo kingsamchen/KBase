6425f5cab86fe232fecb97e32a0e785ed63d95ed
Kingsley Chen <kingsamchen@gmail.com>
2014-11-23 18:19:50 +0800

    add unittest for Get/SetProgram

--- KBase_Test/samples/command_line_unittest.cpp
+++ KBase_Test/samples/command_line_unittest.cpp
@@ -15,3 +15,22 @@ TEST(CommandLineTest, Ctors)
     std::cout << program.AsASCII() << std::endl;
     EXPECT_EQ(program, path);
 }
+
+TEST(CommandLineTest, GetOrSetProgram)
+{
+    kbase::FilePath path(L"C:\\windows\\system32\\calc.exe");
+    kbase::CommandLine cmdline(path);
+    EXPECT_EQ(cmdline.GetProgram().value(), path.value());
+
+    kbase::FilePath path_with_space(L"  C:\\windows\\system32\\calc.exe ");
+    cmdline.SetProgram(path_with_space);
+    EXPECT_EQ(cmdline.GetProgram(), path);
+
+    kbase::FilePath path_with_tabs(L"\tC:\\windows\\system32\\calc.exe\t\t");
+    cmdline.SetProgram(path_with_tabs);
+    EXPECT_EQ(cmdline.GetProgram(), path);
+
+    kbase::FilePath path_with_hybrid(L" \tC:\\windows\\system32\\calc.exe \t ");
+    cmdline.SetProgram(path_with_hybrid);
+    EXPECT_EQ(cmdline.GetProgram(), path);
+}
