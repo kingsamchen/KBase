687030d9ec3428542fc730d9a20c0f8bf998a1f7
Kingsley Chen <kingsamchen@gmail.com>
2014-12-12 18:15:23 +0800

    modified LRUCache 1.change LRUCache template argument Value to Entry 2.change Map in TreeMap & HashMap to MapType. 3.add property fields 4.add Put & erase & Evict.

--- kbase/memory/lru_cache.h
+++ kbase/memory/lru_cache.h
@@ -11,25 +11,27 @@
 
 namespace kbase {
 
+// Standardize type argument signatures of std::map and std::unordered_map.
+
 template<typename Key, typename Value>
 struct TreeMap {
-    using Map = std::map<Key, Value>;
+    using MapType = std::map<Key, Value>;
 };
 
 template<typename Key, typename Value>
 struct HashMap {
-    using Map = std::unordered_map<Key, Value>;
+    using MapType = std::unordered_map<Key, Value>;
 };
 
-template<typename Key, typename Value, template<typename, typename> class Map>
+template<typename Key, typename Entry, template<typename, typename> class Map>
 class LRUCache {
 public:
     using key_type = Key;
-    using value_type = std::pair<const Key, Value>;
+    using value_type = std::pair<const Key, Entry>;
 
 private:
     using CachedEntryList = std::list<value_type>;
-    using KeyTable = typename Map<Key, typename CachedEntryList::iterator>::Map;
+    using KeyTable = typename Map<Key, typename CachedEntryList::iterator>::MapType;
 
 public:
     using size_type = size_t;
@@ -37,7 +39,93 @@ public:
     using const_iterator = typename CachedEntryList::const_iterator;
     using reverse_iterator = typename CachedEntryList::reverse_iterator;
     using const_reverse_iterator = typename CachedEntryList::const_reverse_iterator;
+
+    enum : size_type {
+        NO_AUTO_EVICT = 0
+    };
+
+    explicit LRUCache(size_type max_size)
+        : max_size_(max_size)
+    {}
+
+    LRUCache(const LRUCache&) = delete;
+
+    // TODO: LRUCache(LRUCache&&);
+
+    LRUCache& operator=(const LRUCache&) = delete;
+
+    // TODO: LRUCache& operator=(LRUCache&&);
+
+    ~LRUCache() = default;
+
+    iterator Put(const Key& key, const Entry& entry)
+    {
+        if ((auto key_it = key_table_.find(key)) != key_table_.end()) {
+            erase(key_it->second);
+        } else if (auto_evict() && (max_size() == size())) {
+            Evict();
+        }
+
+        entry_ordering_list_.push_back(value_type(key, entry));
+        key_table_.insert(std::make_pair(key, --entry_ordering_list_.end()));
+    }
+
+    // TODO: iterator Put(const Key& key, Entry&& entry)
+
+    iterator erase(const_iterator pos)
+    {
+        key_table_.erase(pos->first);
+        return entry_ordering_list_.erase(pos);
+    }
+
+    void Evict()
+    {
+        erase(entry_ordering_list_.begin());
+    }
+
+    size_type size() const
+    {
+        return entry_ordering_list_.size();
+    }
+
+    bool empty() const
+    {
+        return entry_ordering_list_.empty();
+    }
+
+    size_type max_size() const
+    {
+        return max_size_;
+    }
+
+    bool auto_evict() const
+    {
+        return max_size_ != 0;
+    }
+
+    iterator begin() { return entry_ordering_list_.begin(); }
+    
+    const_iterator begin() const { return entry_ordering_list_.begin(); }
+    
+    const_iterator cbegin() const { return entry_ordering_list_.cbegin(); }
+
+    iterator end() { return entry_ordering_list_.end(); }
+
+    const_iterator end() const { return entry_ordering_list_.end(); }
+
+    const_iterator end() const { return entry_ordering_list_.cend(); }
+
+private:
+    const size_type max_size_;
+    CachedEntryList entry_ordering_list_;
+    KeyTable key_table_;
 };
+
+template<typename Key, typename Entry>
+using LRUTreeCache = LRUCache<Key, Entry, TreeMap>;
+
+template<typename Key, typename Entry>
+using LRUHashCache = LRUCache<Key, Entry, HashMap>;
 
 }   // namespace kbase
 
