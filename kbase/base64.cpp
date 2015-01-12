6974921302ac0ec458ed4723469a61b81283b04d
kingsleychen <kingsamchen@gmail.com>
2015-01-12 18:25:30 +0800

    base64: interfaces added

--- kbase/base64.cpp
+++ kbase/base64.cpp
@@ -1,5 +1,26 @@
+/*
+ @ Kingsley Chen
+*/
 
 #include "kbase\base64.h"
+
+namespace {
+
+using kbase::byte;
+
+template<typename Container>
+Container Encode(const byte* data, size_t len)
+{
+    return Container();
+}
+
+template<typename Container>
+Container Decode(const byte* data, size_t len)
+{
+    return Container();
+}
+
+}   // namespace
 
 namespace kbase {
 
--- kbase/base64.h
+++ kbase/base64.h
@@ -9,9 +9,19 @@
 #ifndef KBASE_BASE64_H_
 #define KBASE_BASE64_H_
 
+#include <vector>
+
+#include "kbase\basic_types.h"
+
 namespace kbase {
 
+std::vector<byte> Base64Encode(const void* data, size_t len);
 
+std::vector<byte> Base64Decode(const void* data, size_t len);
+
+std::string Base64Encode(const std::string& src);
+
+std::string Base64Decode(const std::string& src);
 
 }   // namespace kbase
 
