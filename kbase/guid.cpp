6b8303c446d6ddb5aec71992ee9ca8d3fa13f4e9
Kingsley Chen <kingsamchen@gmail.com>
2015-01-23 19:13:00 +0800

    GUID: IsGUIDValid working on

--- kbase/guid.cpp
+++ kbase/guid.cpp
@@ -23,15 +23,30 @@ std::string GenerateGUID()
         return std::string();
     }
 
-    const int kGUIDSize = 40;
-    std::array<wchar_t, kGUIDSize> guid_str;
-    int count_written = StringFromGUID2(guid, guid_str.data(), kGUIDSize);
+    const int kGUIDStrSize = 40;
+    std::array<wchar_t, kGUIDStrSize> guid_str;
+    int count_written = StringFromGUID2(guid, guid_str.data(), kGUIDStrSize);
 
     // Since GUID contains ASCII-only characters, it is safe to do this conversion.
     // Strips off { and }.
     return count_written ?
         WideToASCII(WStringPiece(guid_str.data() + 1, count_written - 3)) :
         std::string();
+}
+
+bool IsGUIDValid(const std::string& guid)
+{
+    // Without { prepended and } appended.
+    const size_t kGUIDSize = 36;
+    if (guid.length() != kGUIDSize) {
+        return false;
+    }
+
+    for (size_t i = 0; i < guid.length(); ++i) {
+        if (i == 8 || i == 13 || i == 18 || i == 23) {
+        
+        }
+    }
 }
 
 }   // namespace kbase
