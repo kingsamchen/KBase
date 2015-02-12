8244c2db5c8f3b03e6833914a432e3b483ad527a
Kingsley Chen <kingsamchen@gmail.com>
2015-02-12 14:48:52 +0800

    SHA modified: add some comments.

--- kbase/sha.h
+++ kbase/sha.h
@@ -13,16 +13,29 @@
 
 namespace kbase {
 
+// The functions that perform SHA-1/256 operations rely on crypto API provided by
+// Windows.
+
 using SHA1Digest = std::array<unsigned char, 20>;
 using SHA256Digest = std::array<unsigned char, 32>;
 
-void SHA1Sum(const void* data, size_t size, SHA1Digest* digest);
+// Calculate the SHA-1/256 checksum of a given data.
+// Functions throw an exception if an error occured during invocation of
+// Windows APIs.
 
-std::string SHA1String(const std::string& str);
+void SHA1Sum(const void* data, size_t size, SHA1Digest* digest);
 
 void SHA256Sum(const void* data, size_t size, SHA256Digest* digest);
 
+// Return the SHA-1/256 checksum string, in hexadecimal, of a given string.
+// Functions throw an exception if an error occured during invocation of
+// Windows APIs.
+
+std::string SHA1String(const std::string& str);
+
 std::string SHA256String(const std::string& str);
+
+// Convert a SHA digest into a hexadecimal string.
 
 std::string SHADigestToString(const SHA1Digest& digest);
 
