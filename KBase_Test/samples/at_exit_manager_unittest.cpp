c70c5f827a50370ff9fd594c5cbb5b3e3a5337ab
kingsleychen <kingsamchen@gmail.com>
2015-05-03 00:54:54 +0800

    unittest: add unit test for at exit manager

---
+++ KBase_Test/samples/at_exit_manager_unittest.cpp
@@ -0,0 +1,64 @@
+/*
+ @ Kingsley Chen
+*/
+
+#include "stdafx.h"
+
+#include "gtest\gtest.h"
+#include "kbase\at_exit_manager.h"
+
+namespace {
+
+int g_counter_1 = 0;
+int g_counter_2 = 0;
+
+void IncreaseCounter1(int val)
+{
+    g_counter_1 += val;
+}
+
+void IncreaseCounter2(int val)
+{
+    g_counter_2 += val;
+}
+
+void ZeroCounters()
+{
+    g_counter_1 = 0;
+    g_counter_2 = 0;
+}
+
+}
+
+// WARNING: The following code about how `AtExitManager` is used is technically wrong,
+// and they exist only for test.
+// Should **never** use `AtExitManager` like that in product code.
+
+TEST(AtExitManagerTest, Normal)
+{
+    {
+        kbase::AtExitManager exit_manager;
+        kbase::AtExitManager::RegisterCallback(std::bind(IncreaseCounter1, 2));
+        kbase::AtExitManager::RegisterCallback(std::bind(IncreaseCounter2, 1));
+        EXPECT_EQ(g_counter_1, 0);
+        EXPECT_EQ(g_counter_2, 0);
+    }
+
+    EXPECT_EQ(g_counter_1, 2);
+    EXPECT_EQ(g_counter_2, 1);
+
+    ZeroCounters();
+}
+
+TEST(AtExitManagerTest, FILO_Order)
+{
+    kbase::AtExitManager exit_manager;
+    kbase::AtExitManager::RegisterCallback(std::bind(IncreaseCounter1, 2));
+    kbase::AtExitManager::RegisterCallback([]() {
+        EXPECT_EQ(g_counter_1, 0);
+        EXPECT_EQ(g_counter_2, 1);
+    });
+    kbase::AtExitManager::RegisterCallback(std::bind(IncreaseCounter2, 1));
+    ASSERT_EQ(g_counter_1, 0);
+    ASSERT_EQ(g_counter_2, 0);
+}
