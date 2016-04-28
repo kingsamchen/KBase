a39f7f65a39865e05ee20620c0cd64253c1005ce
Kingsley Chen <kingsamchen@gmail.com>
2016-04-29 01:54:33 +0800

    Add tokenizer facility.
    Tokenizer objects can view all tokens in a string delimited by specify delimiters.

---
+++ KBase_Test/samples/tokenizer_unittest.cpp
@@ -0,0 +1,39 @@
+/*
+ @ 0xCCCCCCCC
+*/
+
+#include "stdafx.h"
+#include "gtest/gtest.h"
+
+#include "kbase/basic_macros.h"
+#include "kbase/tokenizer.h"
+
+using kbase::Tokenizer;
+using kbase::WTokenizer;
+
+TEST(TokenizerTest, Ctor)
+{
+	std::string str = "hello, world";
+	Tokenizer tokenizer(str, ", ");
+	auto begin = tokenizer.begin();
+	auto end = tokenizer.end();
+	auto begin_copy = begin;
+	auto end_move = std::move(end);
+	UNREFED_VAR(begin_copy);
+	UNREFED_VAR(end_move);
+}
+
+TEST(TokenizerTest, Iterate)
+{
+	std::string str = "anything that cannot kill you makes you stronger.\n\tsaid by Bruce Wayne\n";
+	std::vector<std::string> exp { "anything", "that", "cannot", "kill", "you", "makes", "you",
+								   "stronger", "said", "by", "Bruce", "Wayne" };
+	Tokenizer tokenizer(str, " .\n\t");
+	size_t i = 0;
+	for (auto&& token : tokenizer) {
+		if (!token.empty()) {
+			EXPECT_EQ(exp[i], token.ToString());
+			++i;
+		}
+	}
+}
---
+++ kbase/tokenizer.h
@@ -0,0 +1,157 @@
+/*
+ @ 0xCCCCCCCC
+*/
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#ifndef KBASE_TOKENIZER_H_
+#define KBASE_TOKENIZER_H_
+
+#include "kbase/error_exception_util.h"
+#include "kbase/string_view.h"
+
+namespace kbase {
+
+// Both `BasicTokenizer` and its `iterator` access source data via `BasicStringView` objects.
+// That is, they just visit the source.
+
+template<typename CharT>
+class BasicTokenizer {
+public:
+	using token_type = BasicStringView<CharT>;
+	class iterator;
+	using const_iterator = iterator;
+
+	explicit constexpr BasicTokenizer(BasicStringView<CharT> str, BasicStringView<CharT> delim)
+		: data_(str), delim_(delim)
+	{}
+
+	BasicTokenizer(const BasicTokenizer&) noexcept = default;
+
+	BasicTokenizer(BasicTokenizer&&) noexcept = default;
+
+	BasicTokenizer& operator=(const BasicTokenizer&) noexcept = default;
+
+	BasicTokenizer& operator=(BasicTokenizer&&) noexcept = default;
+
+	~BasicTokenizer() = default;
+
+	iterator begin() const
+	{
+		return iterator(data_, 0, delim_);
+	}
+
+	iterator end() const
+	{
+		return iterator(data_, data_.length(), delim_);
+	}
+
+private:
+	token_type data_;
+	token_type delim_;
+};
+
+template<typename CharT>
+class BasicTokenizer<CharT>::iterator {
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using token_type = BasicStringView<CharT>;
+	using value_type = token_type;
+	using difference_type = ptrdiff_t;
+	using pointer = const value_type*;
+	using reference = const value_type&;
+
+	iterator(token_type data, size_t offset, token_type delim)
+		: data_(data), delim_(delim), offset_(offset)
+	{
+		GetToken();
+	}
+
+	iterator(const iterator&) noexcept = default;
+
+	iterator(iterator&&) noexcept = default;
+
+	iterator& operator=(const iterator&) noexcept = default ;
+
+	iterator& operator=(iterator&&) noexcept = default;
+
+	~iterator() = default;
+
+	iterator& operator++()
+	{
+		ENSURE(CHECK, offset_ != data_.length()).Require("iterator now is not incrementable");
+		offset_ = next_offset_;
+		GetToken();
+		return *this;
+	}
+
+	iterator operator++(int)
+	{
+		iterator tmp = *this;
+		++(*this);
+		return tmp;
+	}
+
+	reference operator*() const
+	{
+		return current_token_;
+	}
+
+	pointer operator->() const
+	{
+		return &current_token_;
+	}
+
+	friend bool operator==(const iterator& lhs, const iterator& rhs)
+	{
+		return lhs.data_.data() == rhs.data_.data() &&
+			   lhs.data_.length() == rhs.data_.length() &&
+			   lhs.offset_ == rhs.offset_;
+	}
+
+	friend bool operator!=(const iterator& lhs, const iterator& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+private:
+	void GetToken()
+	{
+		if (offset_ == data_.length()) {
+			current_token_ = token_type();
+			next_offset_ = data_.length();
+			return;
+		}
+
+		size_t token_begin = data_.find_first_not_of(delim_, offset_);
+		if (token_begin == token_type::npos) {
+			current_token_ = token_type();
+			next_offset_ = data_.length();
+			return;
+		}
+
+		size_t token_end = data_.find_first_of(delim_, token_begin);
+		if (token_end == token_type::npos) {
+			token_end = data_.length();
+		}
+
+		current_token_ = token_type(data_.data() + token_begin, token_end - token_begin);
+		next_offset_ = token_end;
+	}
+
+private:
+	token_type data_;
+	token_type delim_;
+	size_t offset_;
+	token_type current_token_;
+	size_t next_offset_ = 0ULL;
+};
+
+using Tokenizer = BasicTokenizer<char>;
+using WTokenizer = BasicTokenizer<wchar_t>;
+
+}	// namespace kbase
+
+#endif	// KBASE_TOKENIZER_H_
