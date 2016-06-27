9296fad14cb8e56ead3c1821c73e548141f10943
Kingsley Chen <kingsamchen@gmail.com>
2016-06-28 00:03:05 +0800

    basic_macros: Add macros DEFAULT_COPY and DEFAULT_MOVE.
    also changes classname in DISALLOW_* macros from uppercase to lowercase.

--- kbase/basic_macros.h
+++ kbase/basic_macros.h
@@ -9,13 +9,21 @@
 #ifndef KBASE_BASIC_MACROS_H_
 #define KBASE_BASIC_MACROS_H_
 
-#define DISALLOW_COPY(CLASSNAME)                        \
-    CLASSNAME(const CLASSNAME&) = delete;               \
-    CLASSNAME& operator=(const CLASSNAME&) = delete
+#define DISALLOW_COPY(classname)                        \
+    classname(const classname&) = delete;               \
+    classname& operator=(const classname&) = delete
 
-#define DISALLOW_MOVE(CLASSNAME)                        \
-    CLASSNAME(CLASSNAME&&) = delete;                    \
-    CLASSNAME& operator=(CLASSNAME&&) = delete
+#define DISALLOW_MOVE(classname)                        \
+    classname(classname&&) = delete;                    \
+    classname& operator=(classname&&) = delete
+
+#define DEFAULT_COPY(classname)                         \
+    classname(const classname&) = default;              \
+    classname& operator=(const classname&) = default;
+
+#define DEFAULT_MOVE(classname)                         \
+    classname(classname&&) = default;                   \
+    classname& operator=(classname&&) = default;
 
 #define UNREFED_VAR(x) \
     ::kbase::internal::SilenceUnusedVariableWarning(x)
