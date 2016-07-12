e5995b521bbcd3d6bf06f9a7f190bf9c9586ec86
Kingsley Chen <kingsamchen@gmail.com>
2016-07-12 15:02:56 +0800

    unittest: update test case for error_exception_util

--- KBase_Test/samples/error_exception_util_unittest.cpp
+++ KBase_Test/samples/error_exception_util_unittest.cpp
@@ -81,8 +81,21 @@ TEST_F(ErrorExceptionUtilTest, LastError_Message)
 
 TEST_F(ErrorExceptionUtilTest, EnsureMacro)
 {
+    kbase::AlwaysCheckForEnsureInDebug(false);
     std::vector<int> v {1, 2, 3, 4, 5, 6, 7, 8, 9};
     EXPECT_ANY_THROW(EnsureMacroTestIntermediate(v));
+}
+
+TEST_F(ErrorExceptionUtilTest, CaptureLastError)
+{
+    kbase::AlwaysCheckForEnsureInDebug(false);
+    try {
+        HANDLE file = CreateFileW(L"c:\\kc123\\not-exist-file.txt", FILE_GENERIC_READ, FILE_SHARE_READ,
+                                  nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+        ENSURE(RAISE, file != nullptr && file != INVALID_HANDLE_VALUE)(LastError()).Require();
+    } catch (const std::runtime_error& ex) {
+        std::cout << ex.what();
+    }
 }
 
 TEST_F(ErrorExceptionUtilTest, RaiseWithCustomizedException)
@@ -91,5 +104,10 @@ TEST_F(ErrorExceptionUtilTest, RaiseWithCustomizedException)
     auto fn = [&v]() {
         ENSURE(RAISE, !v.empty()).Require<MyException>();
     };
+    auto ffn = [&v]() {
+        ENSURE(RAISE, !v.empty()).Require<MyException>("The vector can't be empty!");
+    };
+
     EXPECT_THROW(fn(), MyException);
+    EXPECT_THROW(ffn(), MyException);
 }
